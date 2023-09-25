c7b01987a2073d7209a062de85e4973ad124bc42
Doug Shearer <dgs414@yahoo.com>
2023-09-25 18:40:12 +0000

    #10 - Created an S3 Bucket and installed a TF Provider

--- main.tf
+++ main.tf
@@ -5,22 +5,35 @@ terraform {
       source = "hashicorp/random"
       version = "3.5.1"
     }
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.17.0"
+    }
   }
 }
 
+provider "aws" {
+  # Configuration options
+}
 provider "random" {
   # Configuration options
 }
 resource "random_string" "bucket_name" {
-  length           = 16
+  lower = true
+  upper = false
+  length           = 32
   special          = false
+}
+resource "aws_s3_bucket" "example" {
+  bucket = random_string.bucket_name.result
 }
 
 output "random_bucket_name" {
-    value = random_string.bucket_name.id
-}
-
-output "random_bucket_name_result" {
     value = random_string.bucket_name.result
 }
+#output "random_bucket_name" {
+#    value = random_string.bucket_name.id
+#}
+
+
   
